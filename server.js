4b62d0f9b2cdc734deef7e41548211d603677ee5
loganbeckham <loganbeckham1@gmail.com>
2022-11-28 17:10:56 -0600

    Collection edit button added

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+// =======================================
+//              DEPENDENCIES
+// =======================================
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
+
+// =======================================
+//              DATABASE
+// =======================================
 const Sample = require('./models/mySamples.js');
 const Collection = require('./models/myCollections.js')
-// const userController = require('./controllers/users_controller.js')
-// app.use('/users', userController)
+
+
+// =======================================
+//              ROUTES
+// =======================================
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
@@ -15,40 +26,60 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 
 
-// Index Post
+////////////////////
+// POST ROUTES
+////////////////////
+
+// save samples
 app.post('/save', (req, res) => {
 	Sample.create(req.body, (err, savedSample) => {
 		res.redirect('/collection')
 	})
 })
 
+// create collections
 app.post('/create', (req, res) => {
 	Collection.create(req.body, (err, savedCollection) => {
 		res.redirect('/collection')
 	})
 })
 
-// Home Page
+
+////////////////////
+// PUT ROUTES
+////////////////////
+
+// rename collection
+app.put('/:id', (req, res) => {
+	Collection.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedModel) => {
+		res.redirect('/collections')
+	})
+})
+
+
+////////////////////
+// GET ROUTES
+////////////////////
+
+// HOMEPAGE
 app.get('/', (req, res)=>{
 	res.render('index.ejs');
 })
 
+// CACHE
+app.get('/collections', getSamples, getCollections, renderForm) 
 
-// Show Route
-app.get('/collection', getSamples, getCollections, renderForm) 
-
-// Show Route Functions
 function getSamples(req, res, next) {
 	Sample.find({}, (err, sampleList) => {
-	res.locals.Sample = sampleList;
-	next();
+		res.locals.Sample = sampleList;
+		next();
 	});
 };
 
 function getCollections(req, res, next) {
 	Collection.find({}, (err, collectionList) => {
-	res.locals.Collection = collectionList;
-	next();
+		res.locals.Collection = collectionList;
+		next();
 	});
 };
 
@@ -56,22 +87,38 @@ function renderForm(req, res) {
 	res.render('show.ejs');
 }
 
-
-// CREATE Route
+// CREATE COLLECTION
 app.get('/create', (req, res) => {
 	res.render('create.ejs');
 })
 
-// DELETE Route
+// EDIT COLLECTION
+app.get('/:id/edit', (req, res) => {
+	Collection.findById(req.params.id, (err, thisCollection) => {
+		res.render('edit.ejs',
+		{
+			Collection: thisCollection
+		})
+	})
+})
+
+
+////////////////////
+// DELETE ROUTES
+////////////////////
+
+// DELETE COLLECTION
 app.delete('/collection/:id', (req, res) => {
     Sample.findByIdAndRemove(req.params.id, (err, data) => {
         res.redirect('/collection');
     });
 })
 
-//////////////////////////////
-// SERVER
-//////////////////////////////
+
+
+// =======================================
+//              SERVER
+// =======================================
 
 mongoose.connect('mongodb+srv://student:uxatSYGYAR8JKlrC@cluster0.aaluezz.mongodb.net/?retryWrites=true&w=majority', () => {
     console.log('connected to mongo');
